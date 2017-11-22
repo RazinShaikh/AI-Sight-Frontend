e4bd34ec6e74f3226f607eeef73fc7948876fd1c
Np <fearhev152400@gmail.com>
2017-11-22 15:09:45 +0800

    Send image to server in JSON.
    Add function sendImage, which takes base64-formatted image and send as JSON request.
    And handleImage function, which handles the data from server.

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -11,7 +11,24 @@ var $$ = Dom7;
 //Add a inline pages
 var mainView = myApp.addView('.view-main', {
     //enable inline pages
-    domCache: true  , 
+    domCache: true  ,
     // Enable dynamic Navbar
     dynamicNavbar: true
 });
+
+function sendImage(image_b64) {
+    console.log("Sending image...");
+    var imgdata = '{ "img" : "' + image_b64 + '" }';
+    $$.ajax({
+      datatype: 'json',
+      type: 'POST',
+      data: imgdata,
+      url: "http://127.0.0.1:8000/detect/",
+      success: handleImage
+    });
+}
+
+function handleImage(img_arg) {
+    var image_json = JSON.parse(img_arg);
+    console.log(image_json);
+}
