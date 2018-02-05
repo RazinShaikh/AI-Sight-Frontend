2ea46c00eda25d222cc2516238ffa82f60e1a8c3
Razin Shaikh <razinshaikh@outlook.com>
2018-02-05 21:28:12 +0530

    Send image file instead of base64, boxes on canvas

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,44 +1,119 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+
 // Initialize app
 var myApp = new Framework7({
     swipePanel:'left',
     material:true,
 });
 
-
-// If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
-//Add a inline pages
 var mainView = myApp.addView('.view-main', {
-    //enable inline pages
     domCache: true  ,
-    // Enable dynamic Navbar
     dynamicNavbar: true
 });
 
-function sendImage(image_b64) {
-    console.log("Sending image...");
-    var imgdata = '{ "img" : "' + image_b64 + '" }';
-    $$.ajax({
-      datatype: 'json',
-      type: 'POST',
-      data: imgdata,
-      url: "http://192.168.137.1:1234/detect/",
-      success: handleImage
+function onDeviceReady() {
+    // Now safe to use device APIs
+
+    $$(document).on("click", "#getPhoto", function () {
+        
+        navigator.camera.getPicture(displayImage, errorCallback, {
+            quality: 50,
+            saveToPhotoAlbum: false, //added
+            destinationType: Camera.DestinationType.FILE_URI,
+            correctOrientation: true
+        });
     });
-}
 
-function handleImage(img_arg) {
-    var image_json = JSON.parse(img_arg);
-    console.log(image_json);
-    $$('#imgShow').attr('src', 'data:image; base64,'+ image_json["img"]);
-}
-
-
-function displayImage(imageData) {
-    mainView.router.load({
-        pageName: 'send'
+    $$(document).on("click", "#openAlbum", function () {
+        
+        navigator.camera.getPicture(displayImage, errorCallback, {
+            quality: 50,
+            sourceType : Camera.PictureSourceType.SAVEDPHOTOALBUM,
+            destinationType: Camera.DestinationType.FILE_URI,
+            correctOrientation: true
+        });
     });
-    $$('#imgShow').attr('src', 'data:image; base64,'+imageData);
-    $$('#sendButton').onclick(sendImage(imageData));
+
+
+    function displayImage(fileUri) {
+        mainView.router.load({pageName: 'send'});
+        $$('#imgShow').attr('src', fileUri);
+
+        $$('#sendButton').on("click", function() {
+            $$('#sendButton').hide();
+            myApp.showIndicator();
+            sendImage(fileUri);
+        });
+    }
+
+    function sendImage(fileUri) {
+        window.resolveLocalFileSystemURL(
+            fileUri,
+            function (fileEntry) {
+                fileEntry.file(function (file) {
+                    var reader = new FileReader();
+
+                    reader.onloadend = function() {
+                        var blob = new Blob([new Uint8Array(this.result)], { type: "image/png" });
+                        var oReq = new XMLHttpRequest();
+                        oReq.open("POST", "http://192.168.0.105:8000/img3/", true);
+
+                        oReq.onload = function (oEvent) {
+                            showDetectionResult(oReq.response);
+                        };
+                        // Pass the blob in to XHR's send method
+                        oReq.send(blob);
+                    };
+
+                    reader.readAsArrayBuffer(file);
+
+                }, errorCallback);
+            },
+            errorCallback);
+    }
+
+    function showDetectionResult(img_arg) {
+        
+        var imageJSON = JSON.parse(img_arg);
+        var boxes = imageJSON["boxes"];
+        var scores = imageJSON["scores"];
+        var classes = imageJSON["classes"];
+        var canvas = document.getElementById('myCanvas');
+        var ctx = canvas.getContext('2d');
+
+        canvas.width = $$("#imgShow").width();
+        canvas.height = $$("#imgShow").height();
+
+        drawBoxes(ctx, boxes, scores, classes, canvas.width, canvas.height);
+
+        myApp.hideIndicator();
+    }
+
+    function drawBoxes(ctx, boxes, scores, classes, canvas_width, canvas_height) {
+        var i, x, y, box_width, box_height;
+
+        ctx.lineWidth = "5";
+        ctx.strokeStyle = "blue";
+
+        for (i = 0; i < boxes.length; i++) {
+
+            x = boxes[i][1] * canvas_width;
+            y = boxes[i][0] * canvas_height;
+            box_width = (boxes[i][3] - boxes[i][1]) * canvas_width;
+            box_height = (boxes[i][2] - boxes[i][0]) * canvas_height;
+
+            ctx.beginPath();
+            ctx.rect(x, y, box_width, box_height);
+            ctx.stroke();
+        }
+    }
+
+
+
+    function errorCallback(err) {
+        //uncomment for debugging.
+        //alert("error: "+err);
+    }
 }
