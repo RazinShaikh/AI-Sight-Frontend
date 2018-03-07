9bcd711cae6c59c9c19d2296cafa37f1e1078bff
Np <fearhev152400@gmail.com>
2018-03-07 15:35:11 +0800

    Migrate repository from cs.nott.ac.uk GitLab server to GitLab global.

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,44 +1,177 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+
 // Initialize app
 var myApp = new Framework7({
     swipePanel:'left',
     material:true,
 });
 
-
-// If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
-//Add a inline pages
 var mainView = myApp.addView('.view-main', {
-    //enable inline pages
     domCache: true  ,
-    // Enable dynamic Navbar
     dynamicNavbar: true
 });
 
-function sendImage(image_b64) {
-    console.log("Sending image...");
-    var imgdata = '{ "img" : "' + image_b64 + '" }';
-    $$.ajax({
-      datatype: 'json',
-      type: 'POST',
-      data: imgdata,
-      url: "http://192.168.137.1:1234/detect/",
-      success: handleImage
+function onDeviceReady() {
+    // Now safe to use device APIs
+
+    // document.addEventListener('backbutton', function (e) {
+    //     mainView.router.back();
+    // });
+
+
+    $$(document).on("click", "#getPhoto", function () {
+
+        navigator.camera.getPicture(displayImage, errorCallback, {
+            quality: 50,
+            saveToPhotoAlbum: false, //added
+            destinationType: Camera.DestinationType.FILE_URI,
+            correctOrientation: true
+        });
     });
-}
 
-function handleImage(img_arg) {
-    var image_json = JSON.parse(img_arg);
-    console.log(image_json);
-    $$('#imgShow').attr('src', 'data:image; base64,'+ image_json["img"]);
-}
+    $$(document).on("click", "#openAlbum", function () {
 
-
-function displayImage(imageData) {
-    mainView.router.load({
-        pageName: 'send'
+        navigator.camera.getPicture(displayImage, errorCallback, {
+            quality: 50,
+            sourceType : Camera.PictureSourceType.SAVEDPHOTOALBUM,
+            destinationType: Camera.DestinationType.FILE_URI,
+            correctOrientation: true
+        });
     });
-    $$('#imgShow').attr('src', 'data:image; base64,'+imageData);
-    $$('#sendButton').onclick(sendImage(imageData));
+
+
+    function displayImage(fileUri) {
+        // mainView.router.load({pageName: 'send'});
+        myApp.popup('.popup');
+        $$('#imgShow').attr('src', fileUri);
+
+        $$('#sendButton').on("click", function() {
+            $$('#sendButton').hide();
+            myApp.showIndicator();
+            sendImage(fileUri);
+        });
+    }
+
+    function sendImage(fileUri) {
+        console.log("testt");
+        window.resolveLocalFileSystemURL(
+            fileUri,
+            function (fileEntry) {
+                fileEntry.file(function (file) {
+                    var reader = new FileReader();
+
+                    reader.onloadend = function() {
+                        console.log("testttttttt");
+                        var blob = new Blob([new Uint8Array(this.result)], { type: "image/png" });
+                        var oReq = new XMLHttpRequest();
+                        oReq.open("POST", "http://192.168.0.101:8000/img3/", true);
+
+                        oReq.onload = function (oEvent) {
+                            showDetectionResult(oReq.response);
+                        };
+                        // Pass the blob in to XHR's send method
+                        oReq.send(blob);
+                    };
+
+                    reader.readAsArrayBuffer(file);
+
+                }, errorCallback);
+            },
+            errorCallback);
+    }
+
+    function showDetectionResult(img_arg) {
+
+        var imageJSON = JSON.parse(img_arg);
+        var boxes = imageJSON.boxes;
+        var scores = imageJSON.scores;
+        var classes = imageJSON.classes;
+        var display_string = imageJSON.display_string;
+        var canvas = document.getElementById('myCanvas');
+        var ctx = canvas.getContext('2d');
+
+        canvas.width = $$("#imgShow").width();
+        canvas.height = $$("#imgShow").height();
+
+        drawBoxes(ctx, boxes, scores, classes, display_string, canvas.width, canvas.height);
+
+        myApp.hideIndicator();
+    }
+
+    function drawBoxes(ctx, boxes, scores, classes, display_string, canvas_width, canvas_height) {
+        var i, x, y, box_width, box_height;
+
+        ctx.lineWidth = "5";
+        var std_colors = [
+            'AliceBlue', 'Chartreuse', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque',
+            'BlanchedAlmond', 'BlueViolet', 'BurlyWood', 'CadetBlue', 'AntiqueWhite',
+            'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan',
+            'DarkCyan', 'DarkGoldenRod', 'DarkGrey', 'DarkKhaki', 'DarkOrange',
+            'DarkOrchid', 'DarkSalmon', 'DarkSeaGreen', 'DarkTurquoise', 'DarkViolet',
+            'DeepPink', 'DeepSkyBlue', 'DodgerBlue', 'FireBrick', 'FloralWhite',
+            'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod',
+            'Salmon', 'Tan', 'HoneyDew', 'HotPink', 'IndianRed', 'Ivory', 'Khaki',
+            'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue',
+            'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGrey',
+            'LightGreen', 'LightPink', 'LightSalmon', 'LightSeaGreen', 'LightSkyBlue',
+            'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue', 'LightYellow', 'Lime',
+            'LimeGreen', 'Linen', 'Magenta', 'MediumAquaMarine', 'MediumOrchid',
+            'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen',
+            'MediumTurquoise', 'MediumVioletRed', 'MintCream', 'MistyRose', 'Moccasin',
+            'NavajoWhite', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed',
+            'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed',
+            'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple',
+            'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Green', 'SandyBrown',
+            'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue',
+            'SlateGray', 'SlateGrey', 'Snow', 'SpringGreen', 'SteelBlue', 'GreenYellow',
+            'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet', 'Wheat', 'White',
+            'WhiteSmoke', 'Yellow', 'YellowGreen'
+        ];
+
+        for (i = 0; i < boxes.length; i++) {
+
+            x = boxes[i][1] * canvas_width;
+            y = boxes[i][0] * canvas_height;
+            box_width = (boxes[i][3] - boxes[i][1]) * canvas_width;
+            box_height = (boxes[i][2] - boxes[i][0]) * canvas_height;
+
+            var color = std_colors[classes[i] % std_colors.length];
+            ctx.beginPath();
+
+            ctx.strokeStyle = color;
+            
+            ctx.rect(x, y, box_width, box_height);
+
+            ctx.fillStyle = color;
+
+            var textwidth = ctx.measureText(display_string[i]).width;
+
+            ctx.fillRect(x, y-18, textwidth, 18);
+
+            ctx.fillStyle = 'black';
+
+            ctx.font = "Arial";
+            ctx.fillText(display_string[i], x, y-9);
+            ctx.stroke();
+        }
+    }
+
+
+
+    function errorCallback(err) {
+        //uncomment for debugging.
+        //alert("error: "+err);
+    }
 }
+
+
+//////////////////////////////////////////////////////
+////////////////////////TESTING///////////////////////
+//////////////////////////////////////////////////////
+// var img_arg = '{"boxes":[[0.20688289403915405,0.02382335066795349,0.8024314045906067,0.9165056943893433]],"scores":[0.9100580811500549],"classes":[3],"display_string":["car: 91%"]}';
+
+// displayImage("test.jpg");
+
+// showDetectionResult(img_arg);
